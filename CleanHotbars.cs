fd17dc24f784ee9c46bb05a65629d8f5dac078e1
unknown <alucasxk@gmail.com>
2025-05-31 02:58:27 -0300

    Initial commit: Clean Hotbars mod source and README

---
+++ CleanHotbars.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using UnityEngine;
+using System.Reflection;
+
+namespace clean_hotbars
+{
+    [BepInPlugin("lucasxk.erenshor.cleanhotbars", "Clean Hotbars", "0.8.5")]
+
+    public class CleanHotbars : BaseUnityPlugin
+    {
+        private bool cleaned = false;
+
+        private void Update()
+        {
+            if (cleaned) return;
+
+            var allFound = true;
+            for (int i = 1; i <= 10; i++)
+            {
+                string hkName = $"HK{i}";
+                var hkObj = GameObject.Find($"UI/UIElements/Canvas/HotbarPar/Hotkeys/{hkName}");
+                if (hkObj == null)
+                {
+                    allFound = false;
+                    break;
+                }
+            }
+
+            if (!allFound)
+                return;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                string hkName = $"HK{i}";
+                var hkObj = GameObject.Find($"UI/UIElements/Canvas/HotbarPar/Hotkeys/{hkName}");
+                if (hkObj != null)
+                {
+                    var hkComp = hkObj.GetComponent("Hotkeys");
+                    if (hkComp != null)
+                    {
+                        var hkField = hkComp.GetType().GetField("savedStr", BindingFlags.Instance | BindingFlags.NonPublic);
+                        if (hkField != null)
+                            hkField.SetValue(hkComp, "");
+                    }
+                }
+            }
+
+            cleaned = true;
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+Erenshor Clean Hotbars Mod
+
+This is my first mod ever.
+It removes the big hotbars numbers when no skill/item is assigned.
+It's a simple mod I made for myself with the intention of learning.
+
+## How to install
+
+BepInEx plugin required: https://github.com/BepInEx/BepInEx
+
+
+1. Copy the compiled DLL into BepInEx plugins folder.
+2. Run the game and the mod will automatically clean the hotbar numbers.
+
+Currently the mod is not removing the numbers from the base game secondary hotbar and from Mizuki's Extended Hotbars mod.
+I'm working on it.
